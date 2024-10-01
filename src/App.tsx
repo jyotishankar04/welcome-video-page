da9ff018ae15b585f955dfa2573fbdbe0e0dc030
jyotishankar04 <patrajyotishankar@gamil.com>
2024-10-01 21:21:28 +0530

    added

--- src/App.tsx
+++ src/App.tsx
@@ -5,29 +5,33 @@ import {
   FaVolumeUp,
   FaVolumeMute,
   FaDownload,
-} from "react-icons/fa"; // Importing icons from react-icons
+  FaExpand,
+  FaCompress,
+} from "react-icons/fa";
+const image =
+  "../src/assets/Black and Purple Digital Glitch Tech YouTube Banner.png";
 
 function App() {
   const [isPlaying, setIsPlaying] = useState(false);
-  const [isMuted, setIsMuted] = useState(false); // For managing mute state
-  const [isLoading, setIsLoading] = useState(true); // For showing the loader initially
+  const [isMuted, setIsMuted] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isFullScreen, setIsFullScreen] = useState(false);
   const videoRef = useRef<HTMLVideoElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    // Show loader for 1 second, then attempt to play the video
     const timer = setTimeout(() => {
       if (videoRef.current) {
-        // Play the video with muted sound first
         videoRef.current.muted = isMuted;
         videoRef.current
           .play()
           .then(() => {
             setIsPlaying(true);
-            setIsLoading(false); // Hide loader once video starts playing
+            setIsLoading(false);
           })
           .catch((error) => {
             console.error("Autoplay was prevented:", error);
-            setIsLoading(false); // Hide loader even if autoplay is blocked
+            setIsLoading(false);
           });
       }
     }, 1000);
@@ -60,28 +64,57 @@ function App() {
     if (videoRef.current) {
       const isCurrentlyMuted = videoRef.current.muted;
       videoRef.current.muted = !isCurrentlyMuted;
-      setIsMuted(!isCurrentlyMuted); // Update state based on new mute status
+      setIsMuted(!isCurrentlyMuted);
     }
   };
 
   const handleDownload = () => {
     const link = document.createElement("a");
     link.href = videoUrl;
-    link.download = "carnival_craze_video.mp4"; // Suggested filename
+    link.download = "carnival_craze_video.mp4";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
   };
 
+  const handleFullScreen = () => {
+    if (!isFullScreen) {
+      if (containerRef.current?.requestFullscreen) {
+        containerRef.current.requestFullscreen();
+      } else if (containerRef.current?.webkitRequestFullscreen) {
+        /* Safari */
+        containerRef.current.webkitRequestFullscreen();
+      } else if (containerRef.current?.msRequestFullscreen) {
+        /* IE11 */
+        containerRef.current.msRequestFullscreen();
+      }
+    } else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.webkitExitFullscreen) {
+        /* Safari */
+        document.webkitExitFullscreen();
+      } else if (document.msExitFullscreen) {
+        /* IE11 */
+        document.msExitFullscreen();
+      }
+    }
+    setIsFullScreen(!isFullScreen);
+  };
+
   return (
     <>
-      <div className="w-full h-screen bg-orange-950 items-center gap-8 overflow-auto justify-start flex flex-col">
-        <h1 className="text-4xl text-center text-orange-500 font-bold m-5 ">
+      <div
+        className={`w-full h-screen  items-center bg-orange-950 gap-3 overflow-auto justify-start flex flex-col`}
+      >
+        <h1 className="text-4xl font-mainFont text-center  font-bold m-5">
           CARNIVAL CRAZE 2K24
         </h1>
-        <div className="w-full flex justify-center items-center rounded-lg ">
-          <div className="aspect-[9/16] w-10/12 rounded-lg relative">
-            {/* Loader */}
+        <div className="w-full flex justify-center items-center rounded-lg">
+          <div
+            ref={containerRef}
+            className="aspect-[9/16] pb-8 w-80 sm:w-96 rounded-lg relative"
+          >
             {isLoading && (
               <div className="absolute inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75">
                 <div className="loader border-t-4 border-orange-600 w-12 h-12 rounded-full animate-spin"></div>
@@ -89,8 +122,8 @@ function App() {
             )}
 
             <video
-              className="w-full"
-              muted={isMuted} // Sync mute state with the video
+              className="w-full rounded-lg"
+              muted={isMuted}
               playsInline
               loop
               ref={videoRef}
@@ -101,38 +134,54 @@ function App() {
               Sorry, your browser doesn't support videos.
             </video>
 
-            <div className="flex flex-col w-full gap-3 mt-2">
+            <div className="flex flex-col bg-transparent w-full gap-3 mt-2">
+              <div className="grid grid-cols-2 gap-3">
+                <button
+                  onClick={handlePlayPause}
+                  className="text-white w-full bg-orange-600 py-2 font-bold rounded-lg flex items-center justify-center gap-2 transition hover:bg-orange-700"
+                >
+                  {isPlaying ? (
+                    <>
+                      <FaPause /> Pause
+                    </>
+                  ) : (
+                    <>
+                      <FaPlay /> Play
+                    </>
+                  )}
+                </button>
+                <button
+                  onClick={handleMuteUnmute}
+                  className="text-white w-full bg-orange-600 py-2 font-bold rounded-lg flex items-center justify-center gap-2 transition hover:bg-orange-700"
+                >
+                  {isMuted ? (
+                    <>
+                      <FaVolumeMute /> Unmute
+                    </>
+                  ) : (
+                    <>
+                      <FaVolumeUp /> Mute
+                    </>
+                  )}
+                </button>
+              </div>
               <button
-                onClick={handlePlayPause}
-                className="text-white bg-orange-600 py-2 font-bold rounded-lg flex items-center justify-center gap-2"
+                onClick={handleFullScreen}
+                className="text-white bg-gray-800 py-2 font-bold rounded-lg flex items-center justify-center gap-2 transition hover:bg-gray-900"
               >
-                {isPlaying ? (
+                {isFullScreen ? (
                   <>
-                    <FaPause /> Pause
+                    <FaCompress /> Exit Full Screen
                   </>
                 ) : (
                   <>
-                    <FaPlay /> Play
-                  </>
-                )}
-              </button>
-              <button
-                onClick={handleMuteUnmute}
-                className="text-white bg-orange-600 py-2 font-bold rounded-lg flex items-center justify-center gap-2"
-              >
-                {isMuted ? (
-                  <>
-                    <FaVolumeMute /> Unmute
-                  </>
-                ) : (
-                  <>
-                    <FaVolumeUp /> Mute
+                    <FaExpand /> Full Screen
                   </>
                 )}
               </button>
               <button
                 onClick={handleDownload}
-                className="text-white bg-black py-2 font-bold rounded-lg flex items-center justify-center gap-2"
+                className="text-white bg-black py-2 font-bold rounded-lg flex items-center justify-center gap-2 transition hover:bg-gray-800"
               >
                 <FaDownload /> Download
               </button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mainFont: ["Bungee Tint"],
+        customFont: ["Indie Flower"],
+      }
+      ,backgroundImage:{
+        bgImage: "url('https://res.cloudinary.com/djby1yfko/image/upload/v1727796596/Black_and_Purple_Digital_Glitch_Tech_YouTube_Banner_xqiodu.png')"
+      }
+    },
+    
   },
   plugins: [],
 }
